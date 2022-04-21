6c0f6819e5f66354469c32816dba856e73a29b27
James Brooks <brooksjamesk01@gmail.com>
2022-04-20 18:37:27 -0700

    completed neccessary code to pass the tests

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,8 +1,31 @@
-def read_template():
-  pass
+import re
+# print("""
+# ** Welcome to Madlibs. If you don't know what that is, allow me to explain.                   **
+# ** Madlibs is based on the idea of a user being prompted with a type of word                  **
+# ** (Noun, Adjective, etc.) and based on the inputs will populate a fill in                    **
+# ** the black document to make a story and because the user doesn't know the story beforehand, **
+# ** the result is often humorous to read.                                                      **
+# **                                                                                            **
+# ** The way it will work here once fully fleshed out is it will prompt with a type of word     **
+# ** on the command line and keep prompting until all the blanks have been filled.              **
+# """)
 
-def parse_template():
-  pass
 
-def merge():
-  pass
+def read_template(a):
+    path = f"./{a}"
+    with open(path) as file:
+        contents = file.read() 
+    print('file is closed?', file.closed)
+    return contents
+
+def parse_template(a):
+    stripped_content = re.sub("{(.*?)}", '{}', a)
+    content_parts = re.findall("{(.*?)}", a)
+    unpack_tuple = (*content_parts,)
+    new_tuple = (stripped_content, unpack_tuple)
+    return new_tuple
+
+
+def merge(a, b):
+    story = a.format(*b)
+    return story
